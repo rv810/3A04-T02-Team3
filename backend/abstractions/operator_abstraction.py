6cfa4714fbfe5b8ff75d77c0fbd9033c4212c0ae
Atharva Kulkarni <atharva6905@gmail.com>
2026-04-03 15:33:36 -0400

    fix table name, add missing methods to resolve and acknowledge alerts

--- backend/abstractions/operator_abstraction.py
+++ backend/abstractions/operator_abstraction.py
@@ -4,18 +4,23 @@ from typing import List
 
 class OperatorAbstraction:
     def updateAlertStatus(self, alertID: int, newStatus: str):
-        supabase.table("alerts").update({"status": newStatus}).eq("alertID", alertID).execute()
+        supabase.table("activealerts").update({"status": newStatus}).eq("alertid", alertID).execute()
 
-    def retrieveAcknowledgedAlerts(self, ):
-        response = supabase.table("alerts").select("*").eq("status", "acknowledged").execute()
+    def retrieveAcknowledgedAlerts(self):
+        response = supabase.table("activealerts").select("*").eq("status", "acknowledged").execute()
         return [AlertsInfo(**row) for row in response.data]
-    
+
     def retrieveResolvedAlerts(self):
-        response = supabase.table("alerts").select("*").eq("status", "resolved").execute()
+        response = supabase.table("activealerts").select("*").eq("status", "resolved").execute()
         return [AlertsInfo(**row) for row in response.data]
-    
-    def retrieveAlerts(self) -> List[AlertsInfo]:
+
+    def retrieveActiveAlerts(self) -> List[AlertsInfo]:          
         response = supabase.table("activealerts").select("*").execute()
         return [AlertsInfo(**row) for row in response.data]
 
-    
+    def resolveAlert(self, alertID: int) -> None:               
+        self.updateAlertStatus(alertID, "resolved")
+
+    def acknowledgeAlert(self, alertID: int) -> None:          
+        self.updateAlertStatus(alertID, "acknowledged")
+        

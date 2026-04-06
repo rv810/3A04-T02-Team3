203644e9c68e48f492fa232378a42d1e14a1e879
rv810 <vanir1@mcmaster.ca>
2026-04-05 20:23:54 -0400

    updated CORS with vercel url

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["3a04-t02-team3.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
